e9fb6966c229f9c60839f567862e3ac3e1f71201
Debox-Mehank <mehannk@debox.co.in>
2022-02-03 01:31:03 +0530

    new changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import Layout from "../components/Layout";
 import "swiper/css";
 import "aos/dist/aos.css"
 import Aos from "aos"
+import Head from "next/head";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -16,6 +17,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <title>Debox Consulting - Business Consulting Firm</title>
+        <meta
+          name="description"
+          content="We partner with businesses to build a sustainable growth path through synergies of people, processes and technology."
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
