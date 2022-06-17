55984e9d92c2d1e8b792d3f5ccec952853062518
unknown <meetvyas.dev@gmail.com>
2022-06-17 16:22:38 +0530

    form fix

--- pages/printing/brochure-pamphlets.jsx
+++ pages/printing/brochure-pamphlets.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react';
 import Link from 'next/link'
+import Image from 'next/image'
+// Data
+import imgData from '../../components/printing/imgData';
+// Components
+import PrintingHeader from "../../components/printing/PrintingHeader";
 import PrintingButtons from '../../components/printing/PrintingButtons'
 import HeaderImages from '../../components/printing/HeaderImages'
-import Image from 'next/image'
-import imgData from '../../components/printing/imgData';
 import PrintingInquiryForm from '../../components/printing/PrintingInquiryForm';
 import Carousel from '../../components/swiper/Carousel';
-import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
-import PrintingHeader from "../../components/printing/PrintingHeader";
+import withReactContent from 'sweetalert2-react-content'
+// Hooks
+import { useState, useEffect, Component } from 'react';
+import { useRouter } from 'next/router';
+// Utilities
+import 'react-responsive-modal/styles.css';
+import 'react-phone-number-input/style.css'
+import Input from 'react-phone-number-input/input'
+import PhoneInput from 'react-phone-number-input'
+import Select from 'react-select'
+import Swal from 'sweetalert2'
+const MySwal = withReactContent(Swal)
 
 const brochureImgs = imgData.filter(img => img.category === "brochures-pamphlets")
 
@@ -107,6 +119,21 @@ const BrochurePamphlets = () => {
     const onOpenModal = () => setOpen(true);
     const onCloseModal = () => setOpen(false);
 
+    const router = useRouter()
+    const query = router.query
+
+    useEffect(() => {
+        if (query && query.submit === "true") {
+            MySwal.fire({
+                icon: "success",
+                titleText: "Inquiry Submitted!",
+                html: `<p class="pb-2">Thank you for the inquiry! </p> <p>We'll get back to you shortly!</p>`,
+                timer: 3500,
+                showConfirmButton: false
+            })
+        }
+    }, [query])
+
     return (
         <div className=''>
             <PrintingHeader />
@@ -146,9 +173,106 @@ const BrochurePamphlets = () => {
                                     >
                                         Chat on WhatsApp!
                                     </a>
-
+                                    {/* Props :- productList: options, formName: brochure_inquiry_form */}
                                     <Modal open={open} onClose={onCloseModal} center>
-                                        <PrintingInquiryForm productList={options} formName="brochure_inquiry_form" />
+
+                                        <section className="bg-gray-100 shadow-2xl">
+                                            <div className="max-w-screen-2xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+                                                <div className="">
+                                                    <div className="p-8 bg-white rounded-lg shadow-lg lg:p-12 lg:col-span-3">
+                                                        <form name="brochure_inquiry_form" data-netlify="true" action={`brochure-pamphlets?submit=true`} method="POST" className="space-y-4">
+                                                            <div>
+                                                                <label className="sr-only" htmlFor="name">Full Name</label>
+                                                                <input className="w-full p-3 text-sm md:text-base lg:text-base xl:text-base border-2 border-gray-200 rounded-lg
+                                    " placeholder="Full Name" type="text" id="name" name='fullName' />
+                                                            </div>
+
+                                                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                                                                <div>
+                                                                    <label className="sr-only" htmlFor="email">Email</label>
+                                                                    <input
+                                                                        className="w-full p-3 text-sm md:text-base lg:text-base xl:text-base border-2 border-gray-200 rounded-lg"
+                                                                        placeholder="Email address"
+                                                                        type="email"
+                                                                        id="email"
+                                                                        name='email'
+                                                                    />
+                                                                </div>
+
+                                                                <div>
+                                                                    <label className="sr-only" htmlFor="phone">Phone</label>
+                                                                    {/* <Input
+                                            className="bg-white w-full p-3 rounded text-sm md:text-base border-2 border-gray-200     lg:text-base xl:text-base "
+                                            country="US"
+                                            onChange={() => null}
+                                            placeholder="Phone Number"
+                                        /> */}
+                                                                    <PhoneInput
+                                                                        name="phoneNumber"
+                                                                        placeholder="Enter phone number"
+                                                                        className='bg-white w-full p-3 rounded text-sm md:text-base border-2 border-gray-200     lg:text-base xl:text-base'
+                                                                        onChange={() => null}
+                                                                        defaultCountry="US"
+                                                                        required
+                                                                    />
+                                                                </div>
+                                                            </div>
+
+
+
+                                                            <Select options={options} placeholder="Product(s) Required" isMulti={true} />
+
+
+                                                            <div>
+                                                                <label className="sr-only" htmlFor="message">Message</label>
+                                                                <textarea
+                                                                    className="w-full p-3 text-sm md:text-base lg:text-base xl:text-base border-2 border-gray-200 rounded-lg"
+                                                                    placeholder="Message"
+                                                                    rows="8"
+                                                                    id="message"
+                                                                ></textarea>
+                                                            </div>
+
+
+                                                            <div className='pl-2 flex gap-4'>
+                                                                Urgent Requirement?
+                                                                <div className='flex items-center gap-4'>
+                                                                    <div>
+                                                                        <input className='mx-1' type="radio" name="urgent" id="urgent" />
+                                                                        <label htmlFor="urgent">Yes</label>
+                                                                    </div>
+                                                                    <div>
+                                                                        <input className='mx-1' type="radio" name="urgent" id="not-urgent" />
+                                                                        <label htmlFor="not-urgent">No</label>
+                                                                    </div>
+
+                                                                </div>
+                                                            </div>
+
+                                                            <div className="mt-4">
+                                                                <button
+                                                                    type="submit"
+                                                                    className="inline-flex items-center justify-center w-full px-5 py-3 text-white bg-primary rounded-lg sm:w-auto"
+                                                                >
+                                                                    <span className="font-medium"> Send Enquiry </span>
+
+                                                                    <svg
+                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                        className="w-5 h-5 ml-3"
+                                                                        fill="none"
+                                                                        viewBox="0 0 24 24"
+                                                                        stroke="currentColor"
+                                                                    >
+                                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                                                                    </svg>
+                                                                </button>
+                                                            </div>
+
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </section>
                                     </Modal>
 
 
